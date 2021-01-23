84308c97a594db6e15c65aee3262434c42611901
Joshua Hume <valencelectron@hotmail.com>
2021-01-23 16:52:12 +0900

    created booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_072719) do
+ActiveRecord::Schema.define(version: 2021_01_23_075028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,20 @@ ActiveRecord::Schema.define(version: 2021_01_23_072719) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.boolean "status"
+    t.integer "attendees"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string "location"
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_bookings_on_course_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -59,5 +73,7 @@ ActiveRecord::Schema.define(version: 2021_01_23_072719) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "courses"
+  add_foreign_key "bookings", "users"
   add_foreign_key "courses", "users", column: "users_id"
 end
