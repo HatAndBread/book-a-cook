4c7bd29cb14c5fe6add698238fe06aa79c113f4f
Ryugi Suguro <rsuguro@gmail.com>
2021-01-26 20:55:33 +0900

    Bookings new/create and routes defined

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  def my_bookings
+    @bookings = Booking.where(:user_id == current_user).order(start_time: :ASC)
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = @booking
+
+    if @booking.save
+      redirect_to my_bookings_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.permit(:booking).require(:attendees, :start_time, :end_time, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :bookings, only: [:new, :create] do
+    collection do
+      get "my-bookings", to: "bookings#my_bookings", as: my_bookings
+    end
+  end
 end
