e19bcf08414400eeff69086d4b652c7388e1276e
Joshua Hume <valencelectron@hotmail.com>
2021-01-26 21:19:19 +0900

    made seeds for users and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
-  belongs_to :booking
+  has_many :bookings
   has_many :photos
 
   validates :title, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+100.times do
+  user = User.new
+  user.first_name = Faker::Name.first_name
+  user.last_name = Faker::Name.last_name
+  user.email = Faker::Internet.email
+  user.password = Faker::Internet.password
+  user.save
+end
+
+100.times do
+  course = Course.new
+  course.price = rand(30...100)
+  course.description = Faker::Food.description
+  course.title = Faker::Food.dish
+  course.user = User.all.sample
+  course.user
+  course.save
+end
