4ab569e6c65de616873f86379aa1bb135848e8f5
Joshua Hume <valencelectron@hotmail.com>
2021-01-26 21:42:42 +0900

    added destroy_all to seed and destroy dependents to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-100.times do
+puts "Erasing old junk ✨💃"
+User.destroy_all
+20.times do
   user = User.new
   user.first_name = Faker::Name.first_name
   user.last_name = Faker::Name.last_name
   user.email = Faker::Internet.email
   user.password = Faker::Internet.password
   user.save
+  puts "Randomly generated user:"
+  p user
 end
 
-100.times do
+50.times do
   course = Course.new
   course.price = rand(30...100)
   course.description = Faker::Food.description
@@ -24,4 +27,8 @@ end
   course.user = User.all.sample
   course.user
   course.save
+  puts "Randomly generated course:"
+  p course
 end
+
+puts "Seed finished🐰 Have a lovely day ❣️🧚‍♀️"
