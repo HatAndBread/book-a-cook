59129bf465099fd59b6e525919f67fe55cad9d54
Ryugi Suguro <rsuguro@gmail.com>
2021-01-28 00:17:18 +0900

    homepage view in progress

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module BookACook
   class Application < Rails::Application
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
+
     config.generators do |generate|
       generate.assets false
       generate.helper false
