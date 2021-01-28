b54eb7e5c1f97b5f95ca1849d1521ae738601f65
Joshua Hume <valencelectron@hotmail.com>
2021-01-28 20:25:35 +0900

    added photos to seed. updated user model and course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many :photos
+  has_many_attached :photos
 
   validates :title, presence: true
   validates :price, presence: true
