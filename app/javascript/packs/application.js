2c7ea42dc420358e40c1743bfcfc180e94187248
Chiara Campalani <campalanichiara@gmail.com>
2021-01-30 13:04:17 +0900

    added card fade-up animation on scroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,8 +25,10 @@ import 'bootstrap';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { AOS } from '../components/init_AOS';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  AOS();
 });
