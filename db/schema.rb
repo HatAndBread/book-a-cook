bd82d8214d1fd4efc6876315b42e33ff642c1a9e
Chiara Campalani <campalanichiara@gmail.com>
2021-02-02 22:05:56 +0900

    fix course show form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_111903) do
+ActiveRecord::Schema.define(version: 2021_02_02_114611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,12 +40,12 @@ ActiveRecord::Schema.define(version: 2021_02_02_111903) do
     t.integer "attendees"
     t.datetime "start_time"
     t.datetime "end_time"
-    t.string "location"
     t.bigint "user_id", null: false
     t.bigint "course_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status", default: "pending"
+    t.string "address"
     t.index ["course_id"], name: "index_bookings_on_course_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -107,7 +107,6 @@ ActiveRecord::Schema.define(version: 2021_02_02_111903) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
