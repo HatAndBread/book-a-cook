400a409ad2d19288c01e4d06c90c7fc7d3daa4a9
Joshua Hume <valencelectron@hotmail.com>
2021-02-06 12:53:45 +0900

    added correct avatars to my courses

--- app/javascript/packs/course-show.js
+++ app/javascript/packs/course-show.js
@@ -1,6 +1,1 @@
-// import c from 'chinpunkanpun';
 
-// const reviews = Array.from(document.querySelectorAll('.review-text'));
-// reviews.forEach((review) => {
-//   review.innerText = `${c.sentence()} ${c.sentence()} ${c.sentence()}`;
-// });
